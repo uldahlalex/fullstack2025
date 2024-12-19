427f9275e29656ebbc2373547470c2e1fa607c1b
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-12-19 11:15:35 +0100

    mqtt api part start

---
+++ server/Application/Interfaces/Api.Mqtt/IMessageProcessor.cs
@@ -0,0 +1,14 @@
+public interface IMessageProcessor
+{
+    Task ProcessAsync(Message message);
+}
+
+public interface IMessageDispatcher
+{
+    Task DispatchAsync(Message message, string topic);
+}
+
+public class Message
+{
+    public string MessageString { get; set; }
+}
