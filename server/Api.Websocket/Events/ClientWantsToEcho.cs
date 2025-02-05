914784a032f595fa610817fe8d4f9513d1e42028
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-02-05 12:07:56 +0100

    claims for ws api

--- server/Api.Websocket/Events/ClientWantsToEcho.cs
+++ server/Api.Websocket/Events/ClientWantsToEcho.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Application.Interfaces;
+using Application.Services;
 using Fleck;
 using WebSocketBoilerplate;
 
@@ -7,6 +8,7 @@ namespace Api.Websocket.Events;
 
 public class ClientWantsToEchoDto : BaseDto
 {
+    public string Jwt { get; set; }
     public string Message { get; set; }
 }
 
@@ -15,12 +17,14 @@ public class ServerSendsEchoDto : BaseDto
     public string Message { get; set; }
 }
 
-public class ClientWantsToEcho(IServiceLogic service) : BaseEventHandler<ClientWantsToEchoDto>
+public class ClientWantsToEcho(IServiceLogic service, ISecurityService securityService) : BaseEventHandler<ClientWantsToEchoDto>
 {
     public override Task Handle(ClientWantsToEchoDto dto, IWebSocketConnection socket)
     {
+        var claims = securityService.VerifyJwtOrThrow(dto.Jwt);
         var message = new ServerSendsEchoDto { Message = dto.Message };
         service.Broadcast(message, "");
+        service.GetDomainModels(claims);
         return Task.CompletedTask;
     }
 }
