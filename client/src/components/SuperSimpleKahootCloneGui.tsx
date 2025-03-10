7a7969a96a1653aec78ad40da715b82caf21790b
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-03-10 13:45:51 +0100

    using nswag's client

--- client/src/components/SuperSimpleKahootCloneGui.tsx
+++ client/src/components/SuperSimpleKahootCloneGui.tsx
@@ -1,7 +1,9 @@
 import {useWsClient} from "ws-request-hook";
 import {useEffect, useState} from "react";
 import {randomUid} from "./App.tsx";
+import {VerySimplyKahootWithInMemoryDbClient} from '../generated-client.ts';
 
+export const kahootApi = new VerySimplyKahootWithInMemoryDbClient('http://localhost:8080'); 
 
 export default function SuperSimpleKahootCloneGui() {
     
@@ -38,12 +40,20 @@ export default function SuperSimpleKahootCloneGui() {
                 gameState && <div>Game state: {JSON.stringify(gameState)}</div>
             }
             <hr />
-                <button className="btn" onClick={e => fetch('http://localhost:8080/JoinLobby?clientId='+randomUid, {method: 'POST'})}>Enter lobby</button>
-            <button className="btn" onClick={e => fetch('http://localhost:8080/StartGame', {method: 'POST'})}>Start game for lobby users</button>
-            <button className="btn" onClick={e => fetch('http://localhost:8080/PlayThroughRounds', {method: 'POST'})}>Start next round (if any)</button>
+            {/*    Below are simply using fetch() instead of the nswag definitions*/}
+            {/*    <button className="btn" onClick={e => fetch('http://localhost:8080/JoinLobby?clientId='+randomUid, {method: 'POST'})}>Enter lobby</button>*/}
+            {/*<button className="btn" onClick={e => fetch('http://localhost:8080/StartGame', {method: 'POST'})}>Start game for lobby users</button>*/}
+            {/*<button className="btn" onClick={e => fetch('http://localhost:8080/PlayThroughRounds', {method: 'POST'})}>Start next round (if any)</button>*/}
+            
+            {/*    <span>Answer: <input className="input" value={answer} onChange={e => setAnswer(e.target.value)} /></span>*/}
+            {/*<button className="btn" onClick={e => fetch('http://localhost:8080/SubmitAnswer?player='+randomUid+"&answer="+answer+"&questionId="+gameState.questionId,*/}
+            {/*    {method: 'POST'})}>Submit answer</button>*/}
+
+                <button className="btn" onClick={e => kahootApi.joinLobby(randomUid)}>Enter lobby</button>
+                <button className="btn" onClick={e => kahootApi.startGame()}>Start game for lobby users</button>
+                <button className="btn" onClick={e => kahootApi.playThroughRounds()}>Start next round (if any)</button>
 
                 <span>Answer: <input className="input" value={answer} onChange={e => setAnswer(e.target.value)} /></span>
-            <button className="btn" onClick={e => fetch('http://localhost:8080/SubmitAnswer?player='+randomUid+"&answer="+answer+"&questionId="+gameState.questionId,
-                {method: 'POST'})}>Submit answer</button>
+                <button className="btn" onClick={e => kahootApi.submitAnswer(randomUid, answer, gameState.questionId)}>Submit answer</button>
         </div></>)
 } 
